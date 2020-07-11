e2b12116f5fc689472e7c842e77ec1cee94cd018
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-07-11 09:15:26 -0300

    Questão 01

---
+++ desafio_01.php
@@ -0,0 +1,28 @@
+<?php
+
+function get_real_floor(int $number)
+{
+    if ($number < 0) {
+        return $number;
+    }
+
+    if ($number == 0 || $number == 1) {
+        return 0;
+    }
+
+    $floor = floor($number / 13);
+    return $number - ($floor + 1);
+}
+
+
+$tests = [
+    ['input' => 1, 'output' => 0,],
+    ['input' => 0, 'output' => 0,],
+    ['input' => 5, 'output' => 4,],
+    ['input' => 15, 'output' => 13,],
+    ['input' => -3, 'output' => -3,],
+];
+
+foreach ($tests as $test) {
+    echo (get_real_floor($test['input']) == $test['output'])."<br/>";
+}
